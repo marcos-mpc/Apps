a5b7fabe31ad012bbe3c78a9d1ebb728d58d0d01
Marcos <marcospereirachaves17@gmail.com>
2022-02-28 17:39:58 -0300

    App_02

---
+++ App2_Controle_De_Estoque/funçoes.py
@@ -0,0 +1,21 @@
+from connect_msql import Conector
+
+
+def adicionar():
+    pass
+
+
+def excluir():
+    pass
+
+
+def renomear():
+    pass
+
+
+def entrada_de_produto():
+    pass
+
+
+def saida_de_produto():
+    pass
