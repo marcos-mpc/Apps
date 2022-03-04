71471d0fb60cda4e19cb100479a24620903dc83e
Marcos <marcospereirachaves17@gmail.com>
2022-03-03 22:17:56 -0300

    App_02

--- App2_Controle_De_Estoque/layout.py
+++ App2_Controle_De_Estoque/layout.py
@@ -2,6 +2,12 @@ from tkinter import *
 
 # configurações de tela
 tela = Tk()
+w = tela.winfo_screenwidth()
+h = tela.winfo_screenheight()
+bg = PhotoImage(file='data/BG.png')
+tela['bg'] = '#7fb098'
+label_image = Label(tela, image=bg, bg='#7fb098')
+label_image.place(x=w/4, y=h/9)
 tela.state('zoomed')
 tela.minsize(1320, 660)
 tela.resizable(True, True)
