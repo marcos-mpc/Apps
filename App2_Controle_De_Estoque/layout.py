2559dda56dd1ccf46e36faab4a8548d4d02d8744
Marcos <marcospereirachaves17@gmail.com>
2022-03-05 11:49:10 -0300

    App_02

--- App2_Controle_De_Estoque/layout.py
+++ App2_Controle_De_Estoque/layout.py
@@ -5,18 +5,23 @@ tela = Tk()
 bg = PhotoImage(file='data/BG.png')
 tela['bg'] = '#7fb098'
 label_image = Label(tela, image=bg, bg='#7fb098')
-label_image.place(x=341.5, y=85.33)
+label_image.place(x=350, y=85.33)
 tela.state('zoomed')
 tela.minsize(1320, 660)
 tela.resizable(True, True)
 
 # configurações de botões
-image_entrada = None
-image_saida = None
-image_cosulta = None
-image_novo = PhotoImage(file='data/novo produto.png', height=80, width=80)
+image_entrada = PhotoImage(file='data/entrada.png').subsample(3)
+image_saida = PhotoImage(file='data/saida.png').subsample(3)
+image_cosulta = PhotoImage(file='data/pesquisar.png').subsample(3)
+image_novo = PhotoImage(file='data/novo produto.png').subsample(3)
 
-Button(tela, text='adicionar produto', image=image_novo, compound='top').pack()
+Button(tela, text='cosultar dados', image=image_cosulta, compound='top').place(x=400, y=100)
+Button(tela, text='adicionar produto', image=image_novo, compound='top').place(x=400, y=420)
+Button(tela, text='abastecer', image=image_entrada, compound='top').place(x=800, y=100)
+Button(tela, text='saida', image=image_saida, compound='top').place(x=800, y=420)
 
+
+Label(tela, text='Marcos Pereira Chaves', bg='#7fb098').place(x=650, y=635)
 tela.mainloop()
 
