3c6f8b58e54460389973be26c65832b549bc84cd
Marcos <marcospereirachaves17@gmail.com>
2022-03-04 21:50:37 -0300

    App_02

--- App2_Controle_De_Estoque/layout.py
+++ App2_Controle_De_Estoque/layout.py
@@ -2,13 +2,21 @@ from tkinter import *
 
 # configurações de tela
 tela = Tk()
-w = tela.winfo_screenwidth()
-h = tela.winfo_screenheight()
 bg = PhotoImage(file='data/BG.png')
 tela['bg'] = '#7fb098'
 label_image = Label(tela, image=bg, bg='#7fb098')
-label_image.place(x=w/4, y=h/9)
+label_image.place(x=341.5, y=85.33)
 tela.state('zoomed')
 tela.minsize(1320, 660)
 tela.resizable(True, True)
+
+# configurações de botões
+image_entrada = None
+image_saida = None
+image_cosulta = None
+image_novo = PhotoImage(file='data/novo produto.png', height=80, width=80)
+
+Button(tela, text='adicionar produto', image=image_novo, compound='top').pack()
+
 tela.mainloop()
+
