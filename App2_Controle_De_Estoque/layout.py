79cab0b061722ca25524527c114c04a1d59dc70b
Marcos <marcospereirachaves17@gmail.com>
2022-03-14 18:23:25 -0300

    App_02

--- App2_Controle_De_Estoque/layout.py
+++ App2_Controle_De_Estoque/layout.py
@@ -24,55 +24,55 @@ def cosultar_dados():
     dados.resizable(False, False)
 
 # elementos da tela
-    lb_id = Label(dados, text='ID', font=('verdana', 20, 'bold'))
-    lb_id.place(x=1, y=1)
+    lb_id = Label(dados, text='ID', font=('verdana', 20, 'bold'), relief='sunken', width=4)
+    lb_id.place(x=12, y=75)
 
-    lb_codigo = Label(dados, text='CÓDIGO', font=('verdana', 20, 'bold'))
-    lb_codigo.place(x=55, y=1)
+    lb_codigo = Label(dados, text='CÓDIGO', font=('verdana', 20, 'bold'), relief='sunken', width=8)
+    lb_codigo.place(x=97, y=75)
 
-    lb_produto = Label(dados, text='PRODUTO', font=('verdana', 20, 'bold'))
-    lb_produto.place(x=205, y=1)
+    lb_produto = Label(dados, text='PRODUTO', font=('verdana', 20, 'bold'), relief='sunken', width=9)
+    lb_produto.place(x=258, y=75)
 
-    lb_tipo_unitario = Label(dados, text='TIPO UNITÁRIO', font=('verdana', 20, 'bold'))
-    lb_tipo_unitario.place(x=375, y=1)
+    lb_tipo_unitario = Label(dados, text='TIPO UNITÁRIO', font=('verdana', 20, 'bold'), relief='sunken', width=14)
+    lb_tipo_unitario.place(x=438, y=75)
 
-    lb_quantidade = Label(dados, text='QUANTIDADE', font=('verdana', 20, 'bold'))
-    lb_quantidade.place(x=650, y=1)
+    lb_quantidade = Label(dados, text='QUANTIDADE', font=('verdana', 20, 'bold'), relief='sunken', width=12)
+    lb_quantidade.place(x=713, y=75)
 
-    lb_data = Label(dados, text='ÚLTIMA ATUALIZAÇÃO', font=('verdana', 20, 'bold'))
-    lb_data.place(x=900, y=1)
+    lb_data = Label(dados, text='ÚLTIMA ATUALIZAÇÃO', font=('verdana', 20, 'bold'), relief='sunken', width=20)
+    lb_data.place(x=950, y=75)
 
     lt = obj.mostrar()
     # ID
-    lista = Listbox(dados, relief='solid', font=('verdana', 20))
+    lista = Listbox(dados, relief='solid', font=('verdana', 20), justify=CENTER)
     for c in lt:
         lista.insert(END, c[0])
-    lista.place(x=24, y=250, width=50, height=400)
+    lista.place(x=12, y=120, width=83, height=400)
     # CODIGO
-    lista = Listbox(dados, relief='solid', font=('verdana', 20))
+    lista = Listbox(dados, relief='solid', font=('verdana', 20), justify=CENTER)
     for c in lt:
         lista.insert(END, c[1])
-    lista.place(x=75, y=250, width=75, height=400)
+    lista.place(x=97, y=120, width=159, height=400)
     # PRODUTOS
-    lista = Listbox(dados, relief='solid', font=('verdana', 20))
+    lista = Listbox(dados, relief='solid', font=('verdana', 20), justify=CENTER)
     for c in lt:
         lista.insert(END, c[2])
-    lista.place(x=152, y=250, width=150, height=400)
+    lista.place(x=258, y=120, width=178, height=400)
     # TIPO UNITARIO
-    lista = Listbox(dados, relief='solid', font=('verdana', 20))
+    lista = Listbox(dados, relief='solid', font=('verdana', 20), justify=CENTER)
     for c in lt:
         lista.insert(END, c[3])
-    lista.place(x=304, y=250, width=50, height=400)
+    lista.place(x=438, y=120, width=272, height=400)
     # QUANTIDADE
-    lista = Listbox(dados, relief='solid', font=('verdana', 20))
+    lista = Listbox(dados, relief='solid', font=('verdana', 20), justify=CENTER)
     for c in lt:
         lista.insert(END, c[4])
-    lista.place(x=355, y=250, width=50, height=400)
+    lista.place(x=713, y=120, width=234, height=400)
     # DATA
-    lista = Listbox(dados, relief='solid', font=('verdana', 20))
+    lista = Listbox(dados, relief='solid', font=('verdana', 20), justify=CENTER)
     for c in lt:
         lista.insert(END, c[5])
-    lista.place(x=406, y=250, width=185, height=400)
+    lista.place(x=950, y=120, width=385, height=400)
 
 
 def novo_produto():
