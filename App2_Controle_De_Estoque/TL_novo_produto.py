30ff0c895456306a73b0fb8240ac4a345cdb9944
Marcos <marcospereirachaves17@gmail.com>
2022-05-14 17:43:03 -0300

    App_02

--- App2_Controle_De_Estoque/TL_novo_produto.py
+++ App2_Controle_De_Estoque/TL_novo_produto.py
@@ -5,51 +5,51 @@ from conex_funxoes import adicionar
 class Cadastro:
     def __init__(self):
         self.codigo = StringVar()
-        self.produto = StringVar()
+        self.produtos = StringVar()
         self.tipunit = StringVar()
         self.quantidade = StringVar()
 
-        produto = Toplevel()
-        produto.title('Novo Produto')
-        produto.resizable(False, False)
-        produto.geometry('743x130+300+300')
+        self.produto = Toplevel()
+        self.produto.title('Novo Produto')
+        self.produto.resizable(False, False)
+        self.produto.geometry('743x145+300+300')
 
         # dados da tela
         # labels
-        lb = Label(produto)
-        lb0 = Label(produto, text='CADASTRO DE PRODUTO',
+        lb = Label(self.produto)
+        lb0 = Label(self.produto, text='CADASTRO DE PRODUTO',
                     font=('verdana', 15, 'bold'),
                     relief='ridge',
                     width=52)
-        lb1 = Label(produto, text='CODIGO',
+        lb1 = Label(self.produto, text='CODIGO',
                     font=('verdana', 10, 'bold'),
                     relief='groove',
                     width=20)
-        lb2 = Label(produto, text='PRODUTO',
+        lb2 = Label(self.produto, text='PRODUTO',
                     font=('verdana', 10, 'bold'),
                     relief='groove',
                     width=20)
-        lb3 = Label(produto, text='TIPO UNITARIO',
+        lb3 = Label(self.produto, text='TIPO UNITARIO',
                     font=('verdana', 10, 'bold'),
                     relief='groove',
                     width=20)
-        lb4 = Label(produto, text='QUANTIDADE',
+        lb4 = Label(self.produto, text='QUANTIDADE',
                     font=('verdana', 10, 'bold'),
                     relief='groove',
                     width=20)
 
         # entradas
-        ent1 = Entry(produto, width=30, textvariable=self.codigo)
-        ent2 = Entry(produto, width=30, textvariable=self.produto)
-        ent3 = Entry(produto, width=30, textvariable=self.tipunit)
-        ent4 = Entry(produto, width=30, textvariable=self.quantidade)
+        ent1 = Entry(self.produto, width=30, textvariable=self.codigo)
+        ent2 = Entry(self.produto, width=30, textvariable=self.produtos)
+        ent3 = Entry(self.produto, width=30, textvariable=self.tipunit)
+        ent4 = Entry(self.produto, width=30, textvariable=self.quantidade)
 
         # botoes
-        bt1 = Button(produto, text='Cadastrar',)
+        bt1 = Button(self.produto, text='Cadastrar', command=self.informacoes)
 
-        bt2 = Button(produto,
+        bt2 = Button(self.produto,
                      text='Cancelar',
-                     command=lambda: produto.destroy())
+                     command=lambda: self.produto.destroy())
 
         # layout da tela
         # labels
@@ -69,3 +69,22 @@ class Cadastro:
         lb.grid(row=3, columnspan=4)
         bt1.grid(row=4, column=1, columnspan=2, sticky=E)
         bt2.grid(row=4, column=3, columnspan=2, sticky=W)
+
+    def informacoes(self):
+
+        try:
+            var_codigo = int(self.codigo.get())
+            var_produto = str(self.produtos.get()).upper()
+            var_tipunit = str(self.tipunit.get()).upper()
+            var_quantidade = int(self.quantidade.get())
+            lb5 = Label(self.produto,
+                        text='CADASTRO CONCLUIDO!',
+                        font=('verdana', 10, 'bold'),
+                        relief='groove')
+            lb5.grid(row=5, column=1, columnspan=4)
+        except ValueError:
+            lb5 = Label(self.produto,
+                        text='VALORES INVALIDOS. TENTE NOVAMENTE!',
+                        font=('verdana', 10, 'bold'),
+                        relief='groove')
+            lb5.grid(row=5, column=1, columnspan=4)
